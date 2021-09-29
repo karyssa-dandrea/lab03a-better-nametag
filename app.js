e8f260fe772bbbc682d1cc98da96363a0150f27f
karyssa-dandrea <karyssadandrea@gmail.com>
2021-09-29 13:57:23 -0700

    added color changing buttons for background of id

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
+
+const buttonOrange = document.getElementById('button-orange');
+const buttonGreen = document.getElementById('button-green');
+const buttonPink = document.getElementById('button-pink');
+
+
+
+
+
 // initialize global state
 
 // set event listeners 
@@ -13,3 +22,18 @@ button.addEventListener('click', ()=>{
     let newName = nameInput.value;
     firstName.textContent = newName;
   });
+
+buttonOrange.addEventListener('click', ()=>{
+header.style.backgroundColor = 'orange';
+footer.style.backgroundColor = 'orange';
+})
+
+buttonGreen.addEventListener('click', ()=>{
+header.style.backgroundColor = 'green';
+footer.style.backgroundColor = 'green';
+  })
+
+buttonPink.addEventListener('click', ()=>{
+header.style.backgroundColor = 'pink';
+footer.style.backgroundColor = 'pink';
+      })
